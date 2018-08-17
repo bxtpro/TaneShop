8640819a0acdefa60c285c3db281a64318590681
xuan tan <bxtpro@gmail.com>
2018-08-17 15:06:41 +0700

    thêm model

---
+++ Tane.Model/Abstract/Auditable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tane.Model.Abstract
+{
+    public abstract class Auditable:IAuditable
+    {
+        [MaxLength(250)]
+        public string MetaKeyword { get; set; }
+        [MaxLength(250)]
+        public string MetaDescription { get; set; }
+        public DateTime? CreateDate { get; set; }
+        [MaxLength(50)]
+        public string CreateBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        [MaxLength(50)]
+        public string UpdateBy { get; set; }
+        [Required]
+        public bool Status { get; set; }
+    }
+}
---
+++ Tane.Model/Abstract/IAuditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tane.Model.Abstract
+{
+    interface IAuditable
+    {
+
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+        DateTime? CreateDate { get; set; }
+        string CreateBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+        bool Status { get; set; }
+    }
+}
