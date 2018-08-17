6d1206667843b455c14680d96525f1c6c460673d
xuan tan <bxtpro@gmail.com>
2018-08-18 00:59:17 +0700

    hhh

---
+++ Tane.Data/Respositories/ErrorRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tane.Data.Infastructure;
+using Tane.Model.Models;
+
+namespace Tane.Data.Respositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+
+    }
+    public class ErrorRepository:RepositoryBase<Error>,IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
---
+++ Tane.Web/App_Start/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Owin;
+using Tane.Data;
+using Tane.Data.Infastructure;
+using Tane.Data.Repositories;
+using Tane.Service;
+
+[assembly: OwinStartup(typeof(Tane.Web.App_Start.Startup))]
+
+namespace Tane.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigAutofac(app);
+        }
+
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<TaneDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); //Set the WebApi DependencyResolver
+        }
+    }
+}
